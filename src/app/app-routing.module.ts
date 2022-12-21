5119776391d3927066ec252cfe38bf42864cb9e7
Shwetha K <shwetha.k@happiestminds.com>
2022-12-21 09:11:39 +0530

    donme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './admin/home/home.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { PostComponent } from './admin/dashboard/post/post.component';
+import { CommentsComponent } from './admin/comments/comments.component';
+import { PagesComponent } from './admin/pages/pages.component';
+import { SettingsComponent } from './admin/settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent,
+     children:[
+  { path:'dashboard',component:DashboardComponent },
+  {path:'post',component:PostComponent},
+  {path:'comment',component:CommentsComponent},
+  {path:'pages',component:PagesComponent},
+  {path:'setter',component:SettingsComponent}
+
+]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './admin/home/home.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { SidebarComponent } from './admin/sidebar/sidebar.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { PostComponent } from './admin/dashboard/post/post.component';
+import { CommentsComponent } from './admin/comments/comments.component';
+import { PagesComponent } from './admin/pages/pages.component';
+import { SettingsComponent } from './admin/settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidebarComponent,
+    DashboardComponent,
+    PostComponent,
+    CommentsComponent,
+    PagesComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
